815e9da72d7f140cc6ec8408117a394278a1dcf5
Vijay Raj kumar <88198593+rajkumarvijay@users.noreply.github.com>
2025-12-08 19:46:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,21 +8,24 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidrs" {
-  type    = map(object({
-     cidr_block = string 
-     availability_zone = string 
-}))
+variable "public_subnets" {
+  type = map(object({
+    cidr_block        = string
+    availability_zone = string
+  }))
+
   default = {
-     "public-a" = {
-         cidr_block  = "10.0.1.0/24"
-         availability_zone = "us-east-1"
-     } 
-     "public-b" = {
-         cidr_block = "10.0.2.0/24"
-         availability_zone = "us-east-1"
-     }
+    "public-a" = {
+      cidr_block        = "10.0.1.0/24"
+      availability_zone = "ap-south-1a"
+    }
+    "public-b" = {
+      cidr_block        = "10.0.2.0/24"
+      availability_zone = "ap-south-1b"
+    }
+  }
 }
+
 variable "private_subnet_cidrs" {
   type    = list(string)
   default = ["10.0.11.0/24", "10.0.12.0/24"]
