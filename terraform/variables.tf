eb1d3865ebe4ed2ef7f444b0bae789a577406929
Vijay Raj kumar <88198593+rajkumarvijay@users.noreply.github.com>
2025-12-08 20:15:40 +0530

    Remove default public subnet CIDR values
    Removed default values for public subnets in variables.tf.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,18 +14,6 @@ variable "public_subnets" {
     availability_zone = string
   }))
 
-  default = {
-    "public-a" = {
-      cidr_block        = "10.0.1.0/24"
-      availability_zone = "us-east-1a"
-    }
-    "public-b" = {
-      cidr_block        = "10.0.2.0/24"
-      availability_zone = "us-east-1b"
-    }
-  }
-}
-
 variable "private_subnet_cidrs" {
   type    = list(string)
   default = ["10.0.11.0/24", "10.0.12.0/24"]
