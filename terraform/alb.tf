bfbeccd28ecab95eb50c9ea8f8bb69b6d6d75880
Vijay Raj kumar <88198593+rajkumarvijay@users.noreply.github.com>
2025-12-09 10:41:20 +0530

    Add listener rule for API path routing

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -18,3 +18,19 @@ resource "aws_lb_listener" "listener" {
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
+
+resource "aws_lb_listener_rule" "api_path" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/api/*", "/app/*"]
+    }
+  }
+}
