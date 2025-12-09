09568a3fa795ee3a9a8a83380c1a56511c4cae4a
Vijay Raj kumar <88198593+rajkumarvijay@users.noreply.github.com>
2025-12-09 13:54:06 +0530

    Change egress port range to allow all ports

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -36,8 +36,8 @@ resource "aws_security_group" "task_sg" {
   }
 
   egress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
